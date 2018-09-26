d0344d55529d5f7e38747baf52865acc10976ab4
Alex <alex@wendtedesigns.com>
2018-09-25 20:52:44 -0500

    Implements the top and more than one open props.

--- src/Accordion.js
+++ src/Accordion.js
@@ -2,30 +2,42 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 export default class Accordion extends Component {
+  static getState(moreThanOne, index, openIndexes) {
+    if (moreThanOne) {
+      return [...openIndexes.slice(0, index), !openIndexes[index], ...openIndexes.slice(index + 1, openIndexes.length)]
+    }
+    return openIndexes.map((curItem, curIndex) => index === curIndex)
+  }
+
   state = {
-    openIndex: undefined,
+    // I need to make this the size of the contents array and fill it with false
+    openIndexes: new Array(this.props.items.length).fill(false),
   }
 
   handleClick = index => {
-    this.setState(({ openIndex }) => (openIndex === index ? { openIndex: null } : { openIndex: index }))
+    this.setState(({ openIndexes }) => {
+      const newState = Accordion.getState(this.props.moreThanOneOpen, index, openIndexes)
+      return { openIndexes: newState }
+    })
   }
 
+  Button = ({ item, index }) => (
+    <button className="title" onClick={() => this.handleClick(index)}>
+      {item.title}
+    </button>
+  )
+
+  Content = ({ shouldDisplay, contents }) => (shouldDisplay ? <div className="contents">{contents}</div> : null)
+
   render() {
+    const { top, items } = this.props
     return (
       <div>
-        {this.props.items.map((item, index) => (
-          <div
-            className="item"
-            key={item.title}
-            onKeyDown={() => {
-              this.handleClick(index)
-            }}
-            onClick={() => this.handleClick(index)}
-            role="Button"
-            tabIndex="0"
-          >
-            <p className="title">{item.title}</p>
-            {index === this.state.openIndex && item.contents}
+        {items.map((item, index) => (
+          <div className="item" key={item.title}>
+            {!top && <this.Button item={item} index={index} />}
+            <this.Content shouldDisplay={this.state.openIndexes[index]} contents={item.contents} />
+            {top && <this.Button item={item} index={index} />}
           </div>
         ))}
       </div>
--- src/App.js
+++ src/App.js
@@ -8,11 +8,22 @@ const accordionItems = [
   { title: 'Item 3', contents: <div>Contents of Item 3</div> },
 ]
 
+const Usage = props => (
+  <div>
+    <h3>Content on Top</h3>
+    <Accordion top items={accordionItems} />
+    <h3>Content on Bottom</h3>
+    <Accordion items={accordionItems} />
+    <h3>More than One open</h3>
+    <Accordion moreThanOneOpen items={accordionItems} />
+  </div>
+)
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Accordion items={accordionItems} />
+        <Usage />
       </div>
     )
   }
